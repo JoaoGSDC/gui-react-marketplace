a8acb00ba66bfbb5e3a8c6563e9e1a6ee96e890f
JoaoGSDC <joaogsdc@gmail.com>
2020-09-21 11:04:45 -0300

    Project #9

---
+++ build/precache-manifest.6732364ff6fe91a20bc5a55c9ef0b4eb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98deac04cefabdac5a636b76c11664f1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e8d8c374caeb1818898c",
+    "url": "/static/css/main.25668fe9.chunk.css"
+  },
+  {
+    "revision": "a14f13892e2f9a0bf55f",
+    "url": "/static/js/2.994a146b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.994a146b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8d8c374caeb1818898c",
+    "url": "/static/js/main.6b0746c2.chunk.js"
+  },
+  {
+    "revision": "76df486920c8ce8650ff",
+    "url": "/static/js/runtime-main.a6c1d527.js"
+  },
+  {
+    "revision": "4ddbf19d9097ab53f457affa12e87ab0",
+    "url": "/static/media/logo.4ddbf19d.png"
+  },
+  {
+    "revision": "1334579373e948c5eca9728ac8f49bc5",
+    "url": "/static/media/ninja.13345793.png"
+  }
+]);
